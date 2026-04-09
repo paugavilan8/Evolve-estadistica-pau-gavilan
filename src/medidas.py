5794ee513609ddb5ffe6581a6f5261c9d5bef37d
paugavilan8 <paueto00@hotmail.es>
2026-04-09 16:40:00 +0200

    correccion

--- src/medidas.py
+++ src/medidas.py
@@ -32,6 +32,50 @@ def IQR_evolve(lista_datos: list) -> float:
     q25 = percentil_evolve(lista_datos, 25)
     return round(q75 - q25, 2)
 
+def asimetria_evolve(lista_datos: list) -> float:
+    n = len(lista_datos)
+    media = media_evolve(lista_datos)
+    desviacion = desviacion_evolve(lista_datos)
+    if desviacion == 0:
+        return 0.0
+    asimetria = sum((x - media) ** 3 for x in lista_datos) / n
+    return round(asimetria / (desviacion ** 3), 4)
+
+def curtosis_evolve(lista_datos: list) -> float:
+    n = len(lista_datos)
+    media = media_evolve(lista_datos)
+    desviacion = desviacion_evolve(lista_datos)
+    if desviacion == 0:
+        return 0.0
+    curtosis = sum((x - media) ** 4 for x in lista_datos) / n
+    return round((curtosis / (desviacion ** 4)) - 3, 4)
+
+def outliers_iqr_evolve(lista_datos: list, factor: float = 1.5) -> dict:
+    q1 = percentil_evolve(lista_datos, 25)
+    q3 = percentil_evolve(lista_datos, 75)
+    iqr = IQR_evolve(lista_datos)
+    limite_inferior = q1 - factor * iqr
+    limite_superior = q3 + factor * iqr
+    outliers = [x for x in lista_datos if x < limite_inferior or x > limite_superior]
+    return {
+        "limite_inferior": round(limite_inferior, 2),
+        "limite_superior": round(limite_superior, 2),
+        "num_outliers":    len(outliers),
+        "outliers":        [round(x, 2) for x in sorted(outliers)]
+    }
+
+def outliers_zscore_evolve(lista_datos: list, umbral: float = 3.0) -> dict:
+    media = media_evolve(lista_datos)
+    desviacion = desviacion_evolve(lista_datos)
+    if desviacion == 0:
+        return {"umbral": umbral, "num_outliers": 0, "outliers": []}
+    outliers = [x for x in lista_datos if abs((x - media) / desviacion) > umbral]
+    return {
+        "umbral":       umbral,
+        "num_outliers": len(outliers),
+        "outliers":     [round(x, 2) for x in sorted(outliers)]
+    }
+
 
 
 
@@ -63,6 +107,10 @@ if __name__ == "__main__":
     print(varianza_evolve(edad))
     print(desviacion_evolve(edad))
     print(IQR_evolve(edad))
+    print(asimetria_evolve(edad))
+    print(curtosis_evolve(edad))
+    print(outliers_iqr_evolve(edad))
+    print(outliers_zscore_evolve(edad))
 
     print(media_evolve(salario))
     print(mediana_evolve(salario))
@@ -70,6 +118,10 @@ if __name__ == "__main__":
     print(varianza_evolve(salario))
     print(desviacion_evolve(salario))
     print(IQR_evolve(salario))
+    print(asimetria_evolve(salario))
+    print(curtosis_evolve(salario))
+    print(outliers_iqr_evolve(salario))
+    print(outliers_zscore_evolve(salario))
 
     print(media_evolve(experiencia))
     print(mediana_evolve(experiencia))
@@ -77,3 +129,7 @@ if __name__ == "__main__":
     print(varianza_evolve(experiencia))
     print(desviacion_evolve(experiencia))
     print(IQR_evolve(experiencia))
+    print(asimetria_evolve(experiencia))
+    print(curtosis_evolve(experiencia))
+    print(outliers_iqr_evolve(experiencia))
+    print(outliers_zscore_evolve(experiencia))
